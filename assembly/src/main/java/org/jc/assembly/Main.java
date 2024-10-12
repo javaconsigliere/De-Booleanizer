d33e68de32c067b7531b858adc2691297c3b6394
mnael <javaconsigliere@gmail.com>
2024-10-11 17:14:55 -0700

    Added support for fat jar

---
+++ assembly/src/main/java/org/jc/assembly/Main.java
@@ -0,0 +1,9 @@
+package org.jc.assembly;
+
+import org.jc.chatgpt.CaptureToChatGPT;
+
+public class Main {
+    public static void main(String[] args) {
+        CaptureToChatGPT.main(args);
+    }
+}
